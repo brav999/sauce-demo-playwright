da3b6fa07bd225af28b2c065d1c804570c3944b5
bravi <br.avila.l@gmail.com>
2024-02-19 13:33:19 -0300

    Created .env and adjust the test case example

--- tests/E2E/Login.spec.ts
+++ tests/E2E/Login.spec.ts
@@ -1,11 +1,15 @@
 import { test } from "@playwright/test"
+import { LoginPage } from "../Pages/LoginPage"
 
-test.beforeAll(async () => {
-        
+let loginpage: LoginPage
+
+test.beforeEach(async ({ page }) => {
+    loginpage = new LoginPage(page)
+    await loginpage.go()
 })
 
-test.describe('Teste', async () =>{
-    test('Descrição', async () =>{
+test.describe('Descrição da suíte de testes', async () =>{
+    test('Descrição do test case', async () =>{
         
     })
 })
