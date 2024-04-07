3b4134011f341692506bb6b90ed6432ca468d270
brav999 <brav999@proton.me>
2024-04-07 17:25:03 -0300

    Inclusão de screenshot e novo cenário de teste de inclusão de item no carrinho

---
+++ src/tests/E2E/AddtoCar.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../Pages/LoginPage";
+import { Inventory } from "../Pages/Inventory";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const userValid = process.env.USERVALID ? process.env.USERVALID : "";
+const passValid = process.env.PASSVALID ? process.env.PASSVALID : "";
+
+let loginpage: LoginPage;
+let inventory: Inventory;
+
+test.beforeEach(async ({ page }) => {
+  loginpage = new LoginPage(page);
+  inventory = new Inventory(page);
+
+  await loginpage.go();
+  await loginpage.fillUserAndPass(userValid, passValid);
+});
+
+test.describe("Adicionando itens ao carrinho", async () => {
+  test("Adicionar item ao carrinho", async ({ page }) => {
+    await inventory.addToCart("Sauce Labs Onesie");
+
+    await expect(page.getByText("Sauce Labs Onesie")).toBeVisible();
+    await expect(page.locator('[data-test="remove"]')).toBeVisible();
+  });
+});
---
+++ src/tests/Pages/Inventory.ts
@@ -0,0 +1,20 @@
+import { Page, expect } from "@playwright/test";
+
+export class Inventory {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async addToCart(product: string) {
+    await this.page.getByText(product).click();
+    await this.page.waitForLoadState("networkidle");
+    await this.page.locator('[data-test="add-to-cart"]').click();
+  }
+
+  async validateProduct(product: string) {
+    const item = this.page.getByText(product);
+    expect(item).toBeVisible();
+  }
+}
