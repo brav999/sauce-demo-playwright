c96f74a06379d958c31d0b6ae3fefe1c72a3a6ef
brav999 <br.avila.l@gmail.com>
2024-05-10 17:11:43 -0300

    Validação do carrinho

---
+++ src/Pages/Cart.ts
@@ -0,0 +1,13 @@
+import { Page, expect } from "@playwright/test";
+
+export class Inventory {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async cartConfirmation() {
+    await this.page.getByText("Your Cart").click();
+  }
+}
