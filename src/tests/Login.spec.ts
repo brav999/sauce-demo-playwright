da79541594d1188e960a25582709bd4e8954c6fd
brav999 <br.avila.l@gmail.com>
2024-10-15 19:21:03 -0300

    Refactor user credentials in .env file and update login tests
    - Update USERVALID value in .env file to "standard_users" to reflect the correct user credentials.
    - Refactor login tests to remove logging of test metrics.

--- src/tests/Login.spec.ts
+++ src/tests/Login.spec.ts
@@ -9,10 +9,6 @@ const passInvalid = process.env.PASSINVALID ? process.env.PASSINVALID : '';
 
 let loginpage: LoginPage;
 
-const logMetrics = (data: any) => {
-  fs.appendFileSync('metrics.json', JSON.stringify(data) + '\n');
-};
-
 test.beforeEach(async ({ page }) => {
   loginpage = new LoginPage(page);
   await loginpage.go();
@@ -20,34 +16,22 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Testes de Login', async () => {
   test('Login com usuário e senha validos', async ({ page }) => {
-    const startTimer = Date.now();
     await loginpage.fillUserAndPass(userValid, passValid);
 
     await expect(page).toHaveURL(/.*inventory/);
-
-    const duration = Date.now() - startTimer;
-    logMetrics({ testName: 'Login com dados validos', duration });
   });
 
   test('Login com usuário incorreto', async ({ page }) => {
-    const startTimer = Date.now();
     await loginpage.fillUserAndPass(userInvalid, passValid);
 
     const error = page.locator('[data-test="error"]');
     await expect(error).toBeVisible();
-
-    const duration = Date.now() - startTimer;
-    logMetrics({ testName: 'Login com usuário incorreto', duration });
   });
 
   test('Login com senha inválida', async ({ page }) => {
-    const startTimer = Date.now();
     await loginpage.fillUserAndPass(userValid, passInvalid);
 
     const error = page.locator('[data-test="error"]');
     await expect(error).toBeVisible();
-
-    const duration = Date.now() - startTimer;
-    logMetrics({ testName: 'Login com senha inválida', duration });
   });
 });
